b3577b4da0d0d7863bd1f9b3ffd7d84feef2d49e
Jeff Fitih <39411305+jfitih@users.noreply.github.com>
2018-08-25 16:02:16 -0500

    Add files via upload

---
+++ Arithmatic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithemtic.cs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ArithmeticMethods.cs
+    {
+        public class ArithmeticMethods
+        {
+            //For one this makes nosense to me
+            // I know expect alot but this is ridiculous
+            /* Illustrates the addition, subtraction,
+             * and negation operators
+             * Using named arguments we can pass 
+             * argumenrts by name so that we do not have to remember
+             * their order in the method declaration.
+             * Opitional arguments allow us to specify defalt values
+             * for arguments. If we call and method and do not pass
+             * a value for an opitional argument it will take the default value.
+             * For example, in Example presented we could declare the Multiplicative
+             * method as 
+             * public static void Multiplicative(int x = 25, int y = 14)
+             * so that x has the default value of 25 and y has
+             * the default value of 14. We could then rewrite Main as
+             * public static void Main()
+             * {
+             * Additive(y: 14, x: 25);
+             * Multiplicative();
+             * }
+             * The Additive method hes arguments passed by name so that we may pass them
+             * in a different order than they appear in the declaration.
+             * The Multiplicative  method has opitional arugments so even though we do
+             * not pass any arguments expilictily, x has the default value of 25 and y is 14.
+             */
+            public static void Additive(int x, int y)
+            {
+                int z = x + y;
+                int w = x - y;
+                int p = -y;
+                Console.WriteLine("x + y = {0, 3} x - y = {1, 3} -y = {2, 3} ",
+                    x + y, x - y, -y);
+            }
+            /*Illustrates the multiplication, division, 
+             * and remainder operators
+             */
+            public static void Multiplicative(int x, int y)
+            {
+                int z = x * y;
+                int w = x / 7;
+                int p = x % 7;
+                Console.WriteLine("x * y= {0, 3} x / 7 = {1, 3} x % 7 = {2, 3} ",
+                    x * y, x / 7, x % 7);
+            }
+                                  
+            //Call the methods, which oranizes the code.
+            static void Main(string[] args)
+            {
+
+                int x = 25;
+                int y = 14;
+                Additive(x: 25, y: 14);
+                Multiplicative(x, y);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ PassByValue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByValue.cs
+{
+    class PassByValue
+    {
+        //Returns the cube of its argument
+        public static int Cube(int aNumber) {
+            int result = aNumber * aNumber * aNumber;
+            aNumber += 5;
+            return result;
+        }
+        /*Show that the value in the caller
+         * does not change'
+         */
+        static void Main(string[] args)
+        {
+            int x = 12;  // this is the original code for this
+            int value = Cube(x);
+            Console.WriteLine("The cube of {0} is {1}", x, value);
+            int y = 3;  // this was added to see what was going on
+            int value1 = Cube(y); // Must change the value to value1 or it will read value
+            Console.WriteLine("The cube of {0} is {1}", y, value1);
+            int a = 9;  //Must change variable to (?) or it will read the old one
+            int value2 = Cube(a);
+            Console.WriteLine("The cube of {0} is {1}", a, value2);
+            int b = 6;
+            int value3 = Cube(b);
+            Console.WriteLine("The cube of {0} is {1}", b, value3);
+            int c = 2;
+            int value4 = Cube(c);
+            Console.WriteLine("The cube of {0} is {1}", c, value4);
+            Console.ReadLine(); // Must add to keep on or screen blinks out.
+        }     //So if you do aNumber * aNumber it would square it.
+    }
+}
